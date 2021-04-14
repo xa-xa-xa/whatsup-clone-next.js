379d0d866ff783464b1bed4074d0c87a367e2bdc
Alex K <askrasn@gmail.com>
2021-04-14 11:55:20 -0700

    enable logout

--- components/SideBar/SideBar.jsx
+++ components/SideBar/SideBar.jsx
@@ -1,23 +1,27 @@
 import {useContext, useState} from "react";
 import * as EmailValidator from "email-validator";
-import { auth, db } from "../../firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
+import {auth, db} from "../../firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useCollection} from "react-firebase-hooks/firestore";
 import ChatListItem from "../ChatListItem/ChatListItem";
 import SimpleModal from "../SimpleModal";
 import {Context} from "../../store/reactStore";
-import {Avatar, IconButton, Button, Modal} from "@material-ui/core";
+import {Avatar, IconButton, Button, Modal, List, ListItem, ButtonBase, Popover} from "@material-ui/core";
 import styled from "styled-components";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
 import SearchIcon from "@material-ui/icons/Search";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import {Close, Title} from '@material-ui/icons';
+
 
 const SideBar = () => {
   const [errorModal, setErrorModal] = useState({open: false, text: ""});
-
   const [user] = useAuthState(auth);
+
   const userChatsRef = db
     .collection("chats")
     .where("users", "array-contains", user.email);
@@ -62,20 +66,61 @@ const SideBar = () => {
     setSearchQuery(e)
   }
 
+  // Logout Popover
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+
+  // const handleListItemClick = (value) => {
+  //   onClose(value);
+  // };
+
+
+
   const sideBar = (
     <>
       <Header>
         <CurrentUser>
-          <UserAvatar src={user.photoURL} onClick={() => auth.signOut()} />
+          <UserAvatar src={user.photoURL} />
           <div>{user.email}</div>
         </CurrentUser>
+        <Popover
+          style={{textAlign: "center"}}
+          onClose={handleClose}
+          aria-labelledby="Logout"
+          open={open}
+          onClick={handleClose} >
+          <DialogTitle>Do you want to logout?</DialogTitle>
+          <List>
+            <ListItem
+              key={"logout"}>
+              <CurrentUser disabled>
+                <UserAvatar src={user.photoURL} />
+                <div>{user.email}</div>
+              </CurrentUser>
+              <Button
+                style={{marginLeft: 16}}
+                disableElevation
+                variant="contained"
+                onClick={() => auth.signOut()}>
+                Log out
+                </Button>
+            </ListItem>
+
+          </List>
+        </Popover>
         <IconsContainer>
-          <IconButton>
+          <IconButton onClick={handleClickOpen}>
             <MoreVertIcon />
           </IconButton>
-          <IconButton>
+          {/* <IconButton>
             <ChatIcon />
-          </IconButton>
+          </IconButton> */}
         </IconsContainer>
       </Header>
       <Search>
@@ -108,7 +153,7 @@ const SideBar = () => {
     </>
   );
 
-  const [{ showSidebar }, dispatch] = useContext(Context);
+  const [{showSidebar}, dispatch] = useContext(Context);
   const handleDrawerToggle = () => dispatch({type: "TOGGLE_SIDEBAR"});
   const handleCloseErrorModal = () => {
     setErrorModal({text: "", title: "", open: false});
@@ -145,7 +190,7 @@ const SideBar = () => {
 };
 export default SideBar;
 
-const CreateChatButton = styled(({ ...props }) => (
+const CreateChatButton = styled(({...props}) => (
   <Button {...props} variant="contained" disableElevation={true} />
 ))`
   width: 100%;
@@ -185,7 +230,6 @@ const CurrentUser = styled.div`
 
 export const UserAvatar = styled(Avatar)`
   margin-right: 0.5em;
-  cursor: pointer;
 `;
 const Header = styled.section`
   display: flex;
